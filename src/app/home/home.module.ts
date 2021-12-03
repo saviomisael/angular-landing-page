4cb0510232ed1fee161097601ba7600540440f8d
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-12-03 14:23:45 -0300

    add home module

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavTopComponent } from './navtop/navtop.component';
+
+@NgModule({
+  declarations: [NavTopComponent],
+  imports: [CommonModule],
+})
+export class HomeModule {}
