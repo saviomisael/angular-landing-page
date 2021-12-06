4ddbff855e7b34382e07ad42cb24fb211c8fb718
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-12-06 14:58:07 -0300

    add OtherPackage model

---
+++ src/app/home/shared/models/other-package.model.ts
@@ -0,0 +1,13 @@
+export interface OtherPackage {
+  id: number;
+  name: string;
+  stars_rating_count: number;
+  description: string;
+  location: string;
+  guests_count: number;
+  rooms_count: number;
+  price: number;
+  has_discount: boolean;
+  price_with_discount: number;
+  is_offer: boolean;
+}
