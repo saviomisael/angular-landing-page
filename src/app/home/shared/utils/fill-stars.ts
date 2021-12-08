74388c4077febae481c1e239b174e53bbfc1e6aa
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-12-08 18:21:19 -0300

    remove fill-stars function / remove rxjs-operators file

--- src/app/home/shared/utils/fill-stars.ts
+++
@@ -1,9 +0,0 @@
-export const fillStars = (starsCount: number, content: string): string[] => {
-  const stars: string[] = [];
-
-  for (let index = 0; index < starsCount; index++) {
-    stars.push(content);
-  }
-
-  return stars;
-};
