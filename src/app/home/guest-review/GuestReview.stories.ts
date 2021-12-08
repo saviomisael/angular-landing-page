f7282ecdbb8743cba4c198f5ea84781f25a91a00
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-12-08 18:14:13 -0300

    refactor GuestReviewComponent

--- src/app/home/guest-review/GuestReview.stories.ts
+++ src/app/home/guest-review/GuestReview.stories.ts
@@ -1,10 +1,16 @@
-import { Meta, Story } from '@storybook/angular';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { StarsBoxComponent } from '../stars-box/stars-box.component';
 
 import { GuestReviewComponent } from './guest-review.component';
 
 export default {
   title: 'components/GuestReview',
   component: GuestReviewComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [StarsBoxComponent],
+    }),
+  ],
 } as Meta;
 
 export const Default: Story<GuestReviewComponent> = (args: GuestReviewComponent) => ({
--- src/app/home/guest-review/guest-review.component.ts
+++ src/app/home/guest-review/guest-review.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { fillStars } from '../shared/utils/fill-stars';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-guest-review',
   templateUrl: './guest-review.component.html',
   styleUrls: ['./guest-review.component.scss'],
 })
-export class GuestReviewComponent implements OnInit {
-  stars: string[] = [];
-  private count = 5;
-
-  ngOnInit() {
-    this.stars = fillStars(this.count, '/assets/images/star-rating.svg');
-  }
+export class GuestReviewComponent {
+  count = 5;
 }
