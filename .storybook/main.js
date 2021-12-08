3418f5b0ac1c30d240cb7690915b94a1b2f409f1
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-12-08 16:57:55 -0300

    upgrade storybook to 6.4.8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|ts)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-actions'],
   framework: '@storybook/angular',
   core: {
     builder: 'webpack5',
