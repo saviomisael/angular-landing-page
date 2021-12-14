de97c0bce5e361a7862d90b445c9f8ee6278fa09
Savio Moreira <misaelmoreira99.sm@gmail.com>
2021-12-14 13:49:41 -0300

    move app routing to routes folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './shared/routes/app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 
